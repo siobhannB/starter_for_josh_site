4d98ff6b8e2bd277b17d1976535450aa43eacea5
siobhannB <64455423+siobhannB@users.noreply.github.com>
2021-03-13 23:40:22 +0800

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,30 @@
+<footer class="container-fluid">
+      <div class="container">
+          <div class="row">
+          <div class="col-md-6 text-center footer-content">
+              <p> <?php bloginfo('name'); ?></p>
+              
+              <img src="images/tumblr.jpg" alt="tumblr">
+              <img src="images/rss.jpg" alt="rss">
+              
+              
+              </div>
+              <div class="col-md-6 text-center footer-content">
+              <p>Terms and Conditions</p>
+                  <img src="images/linkedin.jpg" alt="linkedin">
+                  <img src="images/facebook.jpg" alt="facebook">
+              </div>
+          
+          
+          </div>
+         <!-- row-->
+          </div>
+       <!--   container-->
+      </footer>
+      
+      
+
+  
+  </body>
+</html>
+<?php wp_footer(); ?>
