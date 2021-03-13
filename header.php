7337567d0bce236a8970c09e9552dea3296f1788
siobhannB <64455423+siobhannB@users.noreply.github.com>
2021-03-13 23:40:31 +0800

    Update header.php

--- header.php
+++ header.php
@@ -1,14 +1,11 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="<?php bloginfo('charset'); ?>"> 
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-      
-      <link rel="shortcut icon" href="<?php bloginfo('template_directory'); ?>/_/img/favicon.ico">
-      
-    <title>   <?php
+<!doctype html>
+<html>
+<head>
+<meta charset="<?php bloginfo('charset'); ?>">
+
+
+	<title>
+		   <?php
 		      if (function_exists('is_tag') && is_tag()) {
 		         single_tag_title("Tag Archive for &quot;"); echo '&quot; - '; }
 		      elseif (is_archive()) {
@@ -26,54 +23,59 @@
 		      if ($paged>1) {
 		         echo ' - page '. $paged; }
 		   ?>
-</title>
+	</title>
+	
+	
+	<meta name="description" content="<?php bloginfo('description'); ?>">
 
-    <!-- Bootstrap -->
-  
 
-    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
-      
-<link rel="preconnect" href="https://fonts.gstatic.com">
-<link href="https://fonts.googleapis.com/css2?family=Raleway:wght@400;800&display=swap" rel="stylesheet">
-     <?php wp_head(); ?> 
-      </head>
+<!—Viewport responsive  -->
+<meta name="viewport" content="width=device-width, initial-scale=1.0">		
+	
+<link rel="shortcut icon" href="<?php bloginfo('template_directory'); ?>/_/img/favicon.ico">
+	<!-- This is the traditional favicon.
+		 - size: 16x16 or 32x32
+		 - transparency is OK -->
+    <!—Google fonts  -->
+        
+        <link href="https://fonts.googleapis.com/css?family=Raleway:400,800" rel="stylesheet">
+
     
+       
+    <!-- CSS:  -->
+
+        
+	<!—remove all our styles and any Javascript code. Wordpress automatically calls javascript /jquery elsewhere, calls to javascript are not needed in the header or the footer now-->
     
-  <body>
-      <header class="container-fluid herobg">
-      <nav class="navbar">
-  <div class="container-fluid">
-    <div class="navbar-header">
-      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+<?php wp_head(); ?> <!—very important that this is added-->
+
+    </head>
+
+<body>
+    <nav class="navbar ">
+        <div class="container-fluid">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>                        
       </button>
-      <a class="navbar-brand" href="#"><span class="josh">JOSH</span><span class="orange">WHITKIN</span>
-        </a>
-    </div>
-    <div class="collapse navbar-collapse" id="myNavbar">
-      <ul class="nav navbar-nav navbar-right">
-        <li class="active"><a href="#">Home</a></li>
-        <li><a href="#">PROJECTS</a></li>
-        <li><a href="#">PUBLICATIONS</a></li>
-        <li><a href="#">CV</a></li>
-          <li><a href="#">CONTACT</a></li>
-      </ul>
-     
-    </div>
-  </div>
-</nav>
-      
-      
-      <div class="container">
-          <h1> Games Design For the Future</h1>
-           </div> <!--container
-      -->
-    
-      </header>
+                <a class="navbar-brand" href="#">JOSH <span class="orange">WHITKIN</span></a>
+            </div>
+            <div class="collapse navbar-collapse" id="myNavbar">
+                <ul class="nav navbar-nav navbar-right">
+                    <li class="active"><a href="#">HOME</a></li>
+                    <li><a href="#">PROJECTS</a></li>
+                    <li><a href="#">PUBLICATIONS</a></li>
+                    <li><a href="#">CV</a></li>
+                    <li><a href="#">CONTACT</a></li>
+                </ul>
+
+            </div>
+        </div>
+    </nav>
+    <header class="container-fluid  herobg">
+        <div class="container">
+            <h1> Games Design For the future</h1>
+        </div>
+    </header>
