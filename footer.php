2dcf221e2ffaa4543c621cde47a3cd5b75c0628a
siobhannB <64455423+siobhannB@users.noreply.github.com>
2021-03-16 16:18:32 +0800

    Update footer.php

--- footer.php
+++ footer.php
@@ -4,15 +4,15 @@
           <div class="col-md-6 text-center footer-content">
               <p> <?php bloginfo('name'); ?></p>
               
-              <img src="images/tumblr.jpg" alt="tumblr">
-              <img src="images/rss.jpg" alt="rss">
+              <img src="http://206.189.45.97/~mesh5/wp-content/themes/starter_for_josh_site/images/tumblr.jpg" alt="tumblr">
+              <img src="http://206.189.45.97/~mesh5/wp-content/themes/starter_for_josh_site/images/rss.jpg" alt="rss">
               
               
               </div>
               <div class="col-md-6 text-center footer-content">
               <p>Terms and Conditions</p>
-                  <img src="images/linkedin.jpg" alt="linkedin">
-                  <img src="images/facebook.jpg" alt="facebook">
+                  <img src="http://206.189.45.97/~mesh5/wp-content/themes/starter_for_josh_site/images/linkedin.jpg" alt="linkedin">
+                  <img src="http://206.189.45.97/~mesh5/wp-content/themes/starter_for_josh_site/images/facebook.jpg" alt="facebook">
               </div>
           
           
