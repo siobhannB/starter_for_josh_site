ae34eccc91b98ca0d0ac89f096c987f346c6843d
siobhannB <64455423+siobhannB@users.noreply.github.com>
2021-03-18 16:34:09 +0800

    Update index.php

--- index.php
+++ index.php
@@ -37,7 +37,7 @@
                 <div class="col-md-4 article-cent">
                                         <img class="img-responsive" alt=“portrait” src="http://206.189.45.97/~mesh5/wp-content/themes/starter_for_josh_site/images/game.jpg">
 
-                    <h4>A GAMES DESIGN</h4>
+                    <h4>GAMES DESIGN</h4>
                                         <p class="date">12 December 2020</p>
                     <p>STS has never been shy of seeking new terrains of investigation. More and more STS s cholars are starting to explore and intervene in the arts. This object of study brings new challenges and opportunities that we want to explore in this session. We would like to gather first of all simply new kinds of knowledge arising from STS study of the arts.
                     </p>
