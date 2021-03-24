0f001f84fc4ad3541635f62aa0f779c6bdd6f47a
siobhannB <64455423+siobhannB@users.noreply.github.com>
2021-03-24 15:40:34 +0800

    Update functions.php

--- functions.php
+++ functions.php
@@ -6,7 +6,7 @@ wp_enqueue_style( 'style-css', get_template_directory_uri() . '/style.css' );
 wp_enqueue_script('jquery', 'https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js', array(), null, false);
 wp_enqueue_script( 'bootstrap-js', get_template_directory_uri() . '/js/bootstrap.min.js', array(), '3.0.0', true );
 }
- 
+add_theme_support('post-thumbnails'); 
 add_action( 'wp_enqueue_scripts', 'theme_add_bootstrap' );
 
 ?>
