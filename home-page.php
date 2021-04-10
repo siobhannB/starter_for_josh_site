44a7fa08cde721a83e885f044830454e8e222160
siobhannB <64455423+siobhannB@users.noreply.github.com>
2021-04-10 18:07:53 +0800

    Update home-page.php

--- home-page.php
+++ home-page.php
@@ -21,10 +21,16 @@
         <div class="container">
             <div class="row"><!--  a row that gives us access to the BS columns-->
                        <div> 
-                    <h3 class="text-center article-title"> LATEST ARTICLES</h3>
+                    <h3><?php the_field('titleforarticles'); ?></h3>
                 </div>
-                <div class="col-md-4 article-cent">                    
-
+                
+                <div class="col-md-4 article-cent">  
+                    
+                      <img class="img-responsive" alt=“portrait” src="<?php the_field('articleimage'); ?>">
+                      <h4><?php the_field('articletitle'); ?></h4>
+                    <p><?php the_field('articleexcerpt'); ?> </p>
+          <a href="<?php the_field('readmore'); ?>" class="readmore">CONTINUE READING</a>
+                    
                 </div>
                 <div class="col-md-4 article-cent">
 
