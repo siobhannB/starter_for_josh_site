8d1a5d7a2a6394c4651e1f8e71549e9ead8a5007
siobhannB <64455423+siobhannB@users.noreply.github.com>
2021-04-14 05:05:00 +0800

    Update home-page.php

--- home-page.php
+++ home-page.php
@@ -21,7 +21,7 @@
         <div class="container">
             <div class="row"><!--  a row that gives us access to the BS columns-->
                        <div> 
-                    <h3><?php the_field('titleforarticles'); ?></h3>
+                    <h3 class="text-center article-title"><?php the_field('titleforarticles'); ?></h3>
                 </div>
                 
                 <div class="col-md-4 article-cent">  
