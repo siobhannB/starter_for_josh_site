2ab88b9535dbe6c62b0140a51ca3c080bb977351
siobhannB <64455423+siobhannB@users.noreply.github.com>
2021-04-14 18:11:31 +0800

    Update home-page.php

--- home-page.php
+++ home-page.php
@@ -34,10 +34,18 @@
                 </div>
                 <div class="col-md-4 article-cent">
 
-
+     <img class="img-responsive" alt=“model” src="<?php the_field('articleimage1'); ?>">
+                      <h4><?php the_field('articletitle1'); ?></h4>
+                    <p><?php the_field('articleexcerpt1'); ?> </p>
+          <a href="<?php the_field('readmore1'); ?>" class="readmore">CONTINUE READING</a>
+                     
                 </div>
                 <div class="col-md-4 article-cent">
-
+                    
+ <img class="img-responsive" alt=“gamesdesign” src="<?php the_field('articleimage2'); ?>">
+                      <h4><?php the_field('articletitle2'); ?></h4>
+                    <p><?php the_field('articleexcerpt2'); ?> </p>
+          <a href="<?php the_field('readmore2'); ?>" class="readmore">CONTINUE READING</a>
                 </div>
                 
                     </div><!-- row-->
